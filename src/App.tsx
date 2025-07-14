64ea1d449adc8ffb84d90a8a3ede30150f046f71
hubism2511 <unjin95@naver.com>
2025-07-14 15:04:14 +0900

    [#1] feat: Router Setting

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { Router } from './Routes'
 function App() {
   return (
     <>
-      <h1 className="text-red-600 text-3xl font-bold underline">
+      {/* <h1 className="text-red-600 text-3xl font-bold underline">
         설치완료빔입니다.
-      </h1>
+      </h1> */}
       <Router />
     </>
   )
--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
 import { Home, Login } from '@/pages'
+import { MainLayout } from '@/Layouts'
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
